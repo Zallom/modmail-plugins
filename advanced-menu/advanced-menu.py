9a9c583eb1d0a39d6c7eec0f46f4038922357b4d
Zallom <matthias@fca.gg>
2025-07-19 23:38:19 +0200

    fix: Modal reply

--- advanced-menu/advanced-menu.py
+++ advanced-menu/advanced-menu.py
@@ -51,7 +51,12 @@ class CustomModal(discord.ui.Modal):
             responses = {item.label: item.value for item in self.children}
             
             content = "\n".join(f"**{k}**: {v}" for k, v in responses.items())
-            await self.thread.reply(content=content)
+
+            dummyMessage = DummyMessage()
+            dummyMessage.author = self.bot.modmail_guild.me
+            dummyMessage.content = content
+
+            await self.thread.reply(dummyMessage)
             await interaction.response.send_message("Thank you! Your form has been submitted.", ephemeral=True)
         except Exception:
             await interaction.response.send_message("An error occurred.", ephemeral=True)
