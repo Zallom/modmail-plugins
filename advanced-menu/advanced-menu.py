a7e4b7c31a6cc01ac26872910f2363f396781f0d
Zallom <matthias@fca.gg>
2025-07-21 11:32:36 +0200

    fix: self

--- advanced-menu/advanced-menu.py
+++ advanced-menu/advanced-menu.py
@@ -64,8 +64,8 @@ class CustomModal(discord.ui.Modal):
             await self.thread.reply(message=dummyMessage)
             await interaction.response.defer()
 
-            if config["type"] == "command":
-                await invoke_commands(config["callback"], self.bot, self.thread, DummyMessage(copy(self.thread._genesis_message)))
+            if self.config["type"] == "command":
+                await invoke_commands(self.config["callback"], self.bot, self.thread, DummyMessage(copy(self.thread._genesis_message)))
 
         except Exception:
             await interaction.response.send_message("An error occurred.", ephemeral=True)
