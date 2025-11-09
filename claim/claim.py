e71ccd080727b298872a4351dc6ad77d4cbc3672
Zallom <matthias@fca.gg>
2025-11-09 17:10:53 +0100

    fix: use predicate instead of decorator

--- claim/claim.py
+++ claim/claim.py
@@ -41,7 +41,7 @@ class ClaimThread(commands.Cog):
 
         if thread is None:
             await ctx.send('Thread is not claimed')
-        elif str(ctx.author.id) in thread['claimers'] or checks.has_permissions(PermissionLevel.MODERATOR)(ctx):
+        elif str(ctx.author.id) in thread['claimers'] or checks.has_permissions_predicate(PermissionLevel.MODERATOR)(ctx):
             await self.db.delete_one({'thread_id': str(ctx.thread.channel.id)})
             await ctx.send('Unclaimed')
 
@@ -61,7 +61,7 @@ class ClaimThread(commands.Cog):
 
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
 
-        if thread and (str(ctx.author.id) in thread['claimers'] or checks.has_permissions(PermissionLevel.MODERATOR)(ctx)):
+        if thread and (str(ctx.author.id) in thread['claimers'] or checks.has_permissions_predicate(PermissionLevel.MODERATOR)(ctx)):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$addToSet': {'claimers': str(member.id)}})
             await ctx.send('Added to claimers')
 
@@ -73,7 +73,7 @@ class ClaimThread(commands.Cog):
 
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
 
-        if thread and (str(ctx.author.id) in thread['claimers'] or checks.has_permissions(PermissionLevel.MODERATOR)(ctx)):
+        if thread and (str(ctx.author.id) in thread['claimers'] or checks.has_permissions_predicate(PermissionLevel.MODERATOR)(ctx)):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$pull': {'claimers': str(member.id)}})
             await ctx.send('Removed from claimers')
 
@@ -85,14 +85,16 @@ class ClaimThread(commands.Cog):
 
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
 
-        if thread and (str(ctx.author.id) in thread['claimers'] or checks.has_permissions(PermissionLevel.MODERATOR)(ctx)):
+        if thread and (str(ctx.author.id) in thread['claimers'] or checks.has_permissions_predicate(PermissionLevel.MODERATOR)(ctx)):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$set': {'claimers': [str(member.id)]}})
             await ctx.send('Added to claimers')
 
 async def check_reply(ctx):
     thread = await ctx.bot.get_cog('ClaimThread').db.find_one({'thread_id': str(ctx.thread.channel.id)})
+
     if thread:
-        return ctx.author.bot or str(ctx.author.id) in thread['claimers'] or checks.has_permissions(PermissionLevel.MODERATOR)(ctx)
+        return ctx.author.bot or str(ctx.author.id) in thread['claimers'] or checks.has_permissions_predicate(PermissionLevel.MODERATOR)(ctx)
+
     return True
 
 async def setup(bot):
