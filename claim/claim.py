dad362dd2cf13a6c7a681d44962570cb9108f679
Zallom <matthias@fca.gg>
2025-11-09 17:44:09 +0100

    fix: add async processing for check user level function

--- claim/claim.py
+++ claim/claim.py
@@ -67,7 +67,7 @@ class ClaimThread(commands.Cog):
 
         if thread is None:
             await ctx.send('Thread is not claimed')
-        elif str(ctx.author.id) in thread['claimers'] or check_user_level_permissions(ctx):
+        elif str(ctx.author.id) in thread['claimers'] or await check_user_level_permissions(ctx):
             await self.db.delete_one({'thread_id': str(ctx.thread.channel.id)})
             await ctx.send('Unclaimed')
 
@@ -87,7 +87,7 @@ class ClaimThread(commands.Cog):
 
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
 
-        if thread and (str(ctx.author.id) in thread['claimers'] or check_user_level_permissions(ctx)):
+        if thread and (str(ctx.author.id) in thread['claimers'] or await check_user_level_permissions(ctx)):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$addToSet': {'claimers': str(member.id)}})
             await ctx.send('Added to claimers')
 
@@ -99,7 +99,7 @@ class ClaimThread(commands.Cog):
 
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
 
-        if thread and (str(ctx.author.id) in thread['claimers'] or check_user_level_permissions(ctx)):
+        if thread and (str(ctx.author.id) in thread['claimers'] or await check_user_level_permissions(ctx)):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$pull': {'claimers': str(member.id)}})
             await ctx.send('Removed from claimers')
 
@@ -111,7 +111,7 @@ class ClaimThread(commands.Cog):
 
         thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
 
-        if thread and (str(ctx.author.id) in thread['claimers'] or check_user_level_permissions(ctx)):
+        if thread and (str(ctx.author.id) in thread['claimers'] or await check_user_level_permissions(ctx)):
             await self.db.find_one_and_update({'thread_id': str(ctx.thread.channel.id)}, {'$set': {'claimers': [str(member.id)]}})
             await ctx.send('Added to claimers')
 
@@ -119,7 +119,7 @@ async def check_reply(ctx):
     thread = await ctx.bot.get_cog('ClaimThread').db.find_one({'thread_id': str(ctx.thread.channel.id)})
 
     if thread:
-        return ctx.author.bot or str(ctx.author.id) in thread['claimers'] or check_user_level_permissions(ctx)
+        return ctx.author.bot or str(ctx.author.id) in thread['claimers'] or await check_user_level_permissions(ctx)
 
     return True
 
